dbbadf13dfa4482a3c310701c7f1077960c9af76
AykhanTa <aykhanft@code.edu.az>
2024-03-10 18:08:42 +0400

    feat:addition functionalities

--- InterfaceHomeTask/Program.cs
+++ InterfaceHomeTask/Program.cs
@@ -4,13 +4,49 @@
     {
         static void Main(string[] args)
         {
-            //Square square = new(5);
+            Square square = new(5);
             //square.CalcArea();
 
             Rectangle rectangle = new(5, 10);
-            rectangle.CalcArea();
+            //rectangle.CalcArea();
 
+            Console.WriteLine("1.Square");
+            Console.WriteLine("2.Rectangle");
+            Console.WriteLine("0.Quit");
 
+            
+
+            while (true)
+            {
+            TakeCommand:
+                bool result = int.TryParse(Console.ReadLine(), out int command);
+
+                if (!result)
+                {
+                    Console.WriteLine("Duzgun formatda command daxil edin:");
+                    goto TakeCommand;
+
+                }
+                if (command > 0 && command < 3)
+                {
+                    switch (command)
+                    {
+                        case 1:
+                            square.CalcArea();
+                            break;
+                        case 2:
+                            rectangle.CalcArea();
+                            break;
+                    }
+                }
+                else if (command == 0)
+                {
+                    break;
+                }
+
+            }
+            
+     
         }
     }
 }
