83f7c680e4cd68734a44bf8aa08cbbf91b0a59ca
AykhanTa <aykhanft@code.edu.az>
2024-03-10 17:38:49 +0400

    feat:addition Rectangle class

--- InterfaceHomeTask/Program.cs
+++ InterfaceHomeTask/Program.cs
@@ -4,8 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Square square = new(5);
-            square.CalcArea();
+            //Square square = new(5);
+            //square.CalcArea();
+
+            Rectangle rectangle = new(5, 10);
+            rectangle.CalcArea();
+
+
         }
     }
 }
---
+++ InterfaceHomeTask/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿namespace InterfaceHomeTask
+{
+    internal class Rectangle:Figure
+    {
+        int _width;
+        public int Width {
+            get => _width;
+            set 
+            {
+                if (value<0) 
+                {
+                    Console.WriteLine("Teref menfi ola bilmez.");
+                    return;
+                }
+                _width = value;
+            }
+        }
+
+        int _length;
+        public int Length {
+            get => _length;
+            set 
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Teref menfi ola bilmez.");
+                    return;
+                }
+                _length = value;
+
+            } 
+        }
+        public Rectangle(int width,int length)
+        {
+            Width = width;
+            Length = length;
+            
+        }
+
+        public override void CalcArea()
+        {
+            Console.WriteLine(Width*Length);
+        }
+    }
+}
